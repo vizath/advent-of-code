0c01e9937a145224babe14ddfdbf0e192970b10e
Maxime Fafard <maxfafard@gmail.com>
2022-12-05 08:27:47 -0500

    2022 day 3-1

--- 2022/adventofcode.js
+++ 2022/adventofcode.js
@@ -5,42 +5,21 @@ const content = fs.readFileSync("./input.txt");
 const lines = content
   .toString()
   .split("\n")
-  .map((s) => s.split(" "));
+  .map((s) => s.split(""))
+  .map((s) => [s.slice(0, s.length / 2), s.slice(s.length / 2)]);
 console.log("first line:", lines.at(0));
 console.log("last line:", lines.at(-1));
 
-const scores = {
-  A: 1,
-  B: 2,
-  C: 3,
-  // X: 1,
-  // Y: 2,
-  // Z: 3,
-  X: [null, 3, 1, 2],
-  Y: [null, 1, 2, 3],
-  Z: [null, 2, 3, 1],
-};
+const backpacks = lines.map(([c1, c2]) => {
+  const intersection = c1.filter((x) => c2.includes(x));
+  const code = intersection[0].charCodeAt(0);
+  if (code >= 97) {
+    return code - 96
+  } else {
+    return code - 38;
+  }
+})
 
-// const myScores = lines.map((line) => {
-//   const oppScore = scores[line[0]];
-//   const myScore = scores[line[1]];
-
-//   if (oppScore === myScore) {
-//     return myScore + 3;
-//   } else if (oppScore === 3) {
-//     return myScore + (myScore === 1 ? 6 : 0);
-//   } else if (myScore === 3) {
-//     return myScore + (oppScore === 2 ? 6 : 0);
-//   } else {
-//     return myScore + (oppScore === 2 ? 0 : 6);
-//   }
-// });
-
-const myScores = lines.map((line) => {
-  const myScore = scores[line[1]][scores[line[0]]];
-  return myScore + (line[1] === "X" ? 0 : line[1] === "Y" ? 3 : 6);
-});
-
-const answers = myScores.reduce((a, b) => a + b);
+const answers = backpacks.reduce((a, b) => a + b);
 
 console.log("answer", answers);
