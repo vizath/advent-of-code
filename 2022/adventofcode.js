ca94d7fb99e325e48676f2fd02e635133fde3cbc
Maxime Fafard <maxfafard@gmail.com>
2022-12-05 09:25:45 -0500

    2022 day 4-2

--- 2022/adventofcode.js
+++ 2022/adventofcode.js
@@ -21,8 +21,8 @@ console.log("last line:", lines.at(-1));
 
 const isIncluded = lines.filter(([g1, g2]) => {
   return (
-    g1.every((section) => g2.includes(section)) ||
-    g2.every((section) => g1.includes(section))
+    g1.some((section) => g2.includes(section)) ||
+    g2.some((section) => g1.includes(section))
   );
 });
 
