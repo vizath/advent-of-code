ded68a736baaaa2e87115eca626e70c58c392ee0
Maxime Fafard <maxfafard@gmail.com>
2022-12-05 09:56:34 -0500

    2022 day 5-1

--- 2022/adventofcode.js
+++ 2022/adventofcode.js
@@ -4,28 +4,38 @@ const content = fs.readFileSync("./input.txt");
 
 const lines = content
   .toString()
+  .split("\n\n")[1]
   .split("\n")
-  .map((s) => s.split(","))
-  .map((s) =>
-    s.map((sections) => {
-      const n = sections.split("-");
-      const t = [];
-      for (let i = Number(n[0]); i <= Number(n[1]); i++) {
-        t.push(i);
-      }
-      return t;
-    })
-  );
+  .map((s) => /move (\d+) from (\d+) to (\d+)/.exec(s))
+  .map(([_, nb, start, end]) => [Number(nb), Number(start), Number(end)]);
 console.log("first line:", lines.at(0));
 console.log("last line:", lines.at(-1));
 
-const isIncluded = lines.filter(([g1, g2]) => {
-  return (
-    g1.some((section) => g2.includes(section)) ||
-    g2.some((section) => g1.includes(section))
-  );
+const containers = content.toString().split("\n\n")[0].split("\n").reverse();
+containers.shift();
+
+const nbContainers = 9;
+const parsed = Array(nbContainers)
+  .fill(null)
+  .map(() => []);
+containers.forEach((line) => {
+  let col = 0;
+  for (let i = 1; i < line.length; i += 4) {
+    if (line[i] && line[i] !== " ") {
+      parsed[col].push(line[i]);
+    }
+    col++;
+  }
 });
 
-const answers = isIncluded.length;
+lines.forEach(([nb, start, end]) => {
+  for (let i = 0; i < nb; i++) {
+    parsed[end - 1].push(parsed[start - 1].pop());
+  }
+});
+
+console.log(parsed);
+
+const answers = parsed.map((p) => p[p.length - 1]).join("");
 
 console.log("answer", answers);
