1fe3d99492cc40b1f65ba8b20a351567afe788cb
Maxime Fafard <maxfafard@gmail.com>
2022-12-05 08:32:25 -0500

    2022 day 3-2

--- 2022/adventofcode.js
+++ 2022/adventofcode.js
@@ -6,19 +6,28 @@ const lines = content
   .toString()
   .split("\n")
   .map((s) => s.split(""))
-  .map((s) => [s.slice(0, s.length / 2), s.slice(s.length / 2)]);
+  .reduce((carry, b, i) => {
+    if (i % 3 === 0) {
+      carry.push([b]);
+    } else {
+      carry[carry.length - 1].push(b);
+    }
+    return carry;
+  }, []);
 console.log("first line:", lines.at(0));
 console.log("last line:", lines.at(-1));
 
-const backpacks = lines.map(([c1, c2]) => {
-  const intersection = c1.filter((x) => c2.includes(x));
+const backpacks = lines.map(([c1, c2, c3]) => {
+  const intersection = c1
+    .filter((x) => c2.includes(x))
+    .filter((x) => c3.includes(x));
   const code = intersection[0].charCodeAt(0);
   if (code >= 97) {
-    return code - 96
+    return code - 96;
   } else {
     return code - 38;
   }
-})
+});
 
 const answers = backpacks.reduce((a, b) => a + b);
 
