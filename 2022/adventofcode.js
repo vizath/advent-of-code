f0acf0900766ea33fd62f2a68e55f412b4ec720a
Maxime Fafard <maxfafard@gmail.com>
2022-12-07 07:13:54 -0500

    2022 day 7-1

--- 2022/adventofcode.js
+++ 2022/adventofcode.js
@@ -2,25 +2,67 @@ const fs = require("fs");
 
 const content = fs.readFileSync("./input.txt");
 
-const lines = content.toString();
+const lines = content.toString().split("\n");
 console.log("first line:", lines.at(0));
 console.log("last line:", lines.at(-1));
 
-let answers = 0;
-
-const markerLength = 14;
-const line = lines;
-for (let i = markerLength - 1; i < line.length; i++) {
-  if (
-    new Set(
-      Array(markerLength)
-        .fill(0)
-        .map((_, k) => line[i - k])
-    ).size === markerLength
-  ) {
-    answers = i + 1;
-    break;
+const tree = lines.reduce(
+  (carry, line) => {
+    // console.log(line);
+    if (line[0] === "$") {
+      // command
+      const [dollar, command, arg] = line.split(" ");
+      if (command === "cd") {
+        if (arg === "/") {
+          carry.currentDirectory = "/";
+        } else if (arg === "..") {
+          carry.currentDirectory = carry.currentDirectory.substring(
+            0,
+            carry.currentDirectory.lastIndexOf("/")
+          );
+        } else if (carry.currentDirectory === "/") {
+          carry.currentDirectory += arg;
+        } else {
+          carry.currentDirectory += "/" + arg;
+        }
+      } else if (command === "ls") {
+        carry.files[carry.currentDirectory] = [];
+      } else {
+        console.log("what?");
+      }
+    } else if (line[0] === "d") {
+      // directory
+    } else {
+      const [size, filename, other] = line.split(" ");
+      if (other) {
+        console.log("other", line);
+      }
+      carry.files[carry.currentDirectory].push({
+        filename,
+        size: Number(size),
+      });
+    }
+    // console.log(carry);
+    return carry;
+  },
+  {
+    currentDirectory: "/",
+    files: {},
   }
-}
+);
+
+console.log(tree.files);
+const answers = Object.entries(tree.files)
+  .map(([directory]) => {
+    const sudDirectories = Object.keys(tree.files).filter(
+      (d) => `${d}/`.indexOf(`${directory}/`) === 0
+    );
+    return sudDirectories
+      .map((d) => tree.files[d].map((f) => Number(f.size)))
+      .flat()
+      .reduce((c, f) => c + f);
+  })
+  .filter((s) => s <= 100000)
+  .reduce((c, s) => c + s, 0);
 
 console.log("answer", answers);
